281913106e1ef81fa29538c015258038745b86f0
spicata <p.ixelandian@gmail.com>
2024-05-23 21:49:49 +0800

    hi there

--- pressg.py
+++ pressg.py
@@ -4,55 +4,58 @@ from shutil import rmtree
 from pathlib import Path
 import re
 
-if exists("./docs/"):
-    rmtree("./docs/")
-makedirs("./docs/")
-Path('./docs/.nojekyll').touch()
+# defaults (change if you want to configure)
+# if you are on windows, replace all '/' with '\' (ctrl+h)
+outputDir = './docs/'
+fileType = '.ccc'
+baseHtml = './html/main.html'
+replacement = '[[[content]]]'
 
-# print(listdir())
-exhibita = list(walk('.'))
-exhibitb = []
-exhibitccc = []
-exhibite = ''
-# print(exhibita)
+# reset /docs/ and add .nojekyll
+if exists(outputDir):
+    rmtree(outputDir)
+makedirs(outputDir)
+Path(outputDir + '.nojekyll').touch()
+
+
+walkedFilesList = list(walk('.'))
+allFilePaths = []
+cFilePaths = []
+htmlWrapper = ''
+
 x = 0
-for i in exhibita:
+for i in walkedFilesList:
     y = 0
-    # print(exhibita[x])
-    for j in exhibita[x][2]:
-        exhibitb.append(join(exhibita[x][0],exhibita[x][2][y]))
+    for j in walkedFilesList[x][2]:
+        allFilePaths.append(join(walkedFilesList[x][0],walkedFilesList[x][2][y]))
         y += 1
     x += 1
-# top 10 spaghetti code
 
 z = 0
-for k in exhibitb:
-    if exhibitb[z][-4:] == '.ccc' and not exhibitb[z][:7] == "./docs/":
-        exhibitccc.append(exhibitb[z])
-    if exhibitb[z] == "./html/main.html":
-        exhibite = exhibitb[z]
+for k in allFilePaths:
+    if allFilePaths[z][-len(fileType):] == fileType and not allFilePaths[z][:len(outputDir)] == outputDir:
+        cFilePaths.append(allFilePaths[z])
+    if allFilePaths[z] == baseHtml:
+        htmlWrapper = allFilePaths[z]
     z += 1
 
 aa = 0
-for l in exhibitccc:
-    exhibitd = open(exhibitccc[aa]).read()
-    if exhibite:
-        exhibitered = open(exhibite).read()
-        exhibitfinal = exhibitered.replace("[[[content]]]", "<pre>\n" + exhibitd + "\n</pre>")
+for l in cFilePaths:
+    openCFile = open(cFilePaths[aa]).read()
+    if htmlWrapper:
+        tempFinalFile = open(htmlWrapper).read().replace(replacement, "<pre>\n" + openCFile + "\n</pre>")
     else:
-        exhibitfinal = "<pre>\n" + exhibitd + "\n</pre>"
-    pathtowriteto = exhibitccc[aa].replace("./", "./docs/").replace(".ccc", ".html")
-    ptwtdirs = pathtowriteto.split("/")
-    ptwtdirlist = ['.']
+        tempFinalFile = "<pre>\n" + openCFile + "\n</pre>"
+    writeToPath = cFilePaths[aa].replace("./", outputDir).replace(fileType, ".html")
+    writeToDir = writeToPath.split("/")
+    writeToPathAsList = ['.']
     ab = 0
-    for m in ptwtdirs:
-        if ab != 0 and ab != (len(ptwtdirs) - 1):
-            ptwtdirlist.append(ptwtdirs[ab])
-            ptwtdirlistjoin = "/".join(ptwtdirlist)
-            if not exists(ptwtdirlistjoin):
-                makedirs(ptwtdirlistjoin)
+    for m in writeToDir:
+        if ab != 0 and ab != (len(writeToDir) - 1):
+            writeToPathAsList.append(writeToDir[ab])
+            writeToPathAsListjoin = "/".join(writeToPathAsList)
+            if not exists(writeToPathAsListjoin):
+                makedirs(writeToPathAsListjoin)
         ab += 1
-    open(pathtowriteto, "w+").write(exhibitfinal)
+    open(writeToPath, "w+").write(tempFinalFile)
     aa += 1
-
-# top 2 spaghetti code
